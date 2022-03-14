f14d36c4be604366753e1c760e46194172ce9bd9
root <root@kadeploy.common.lip6.fr>
2022-03-14 16:39:08 +0100

    Last run at lun. 14 mars 2022 16:39:08 CET

--- 16 02 QSFK Sweep/Q_class.py
+++ 16 02 QSFK Sweep/Q_class.py
@@ -18,7 +18,7 @@ class Queue:
         self.T_prob = tran_prob # Transmission probability
         self.demands = 0; # Requests, initialized to zero
         #self.rng = np.random.default_rng(seed=4529)
-        self.rng = KnockoffNpRandom(seed = 9198)
+        self.rng = KnockoffNpRandom()
         
     def SetPhysical(self,arr_rate_s,t_step):
         self.type = "physical"
